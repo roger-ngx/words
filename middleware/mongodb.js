179853f36d5873b57e7cfcfda12379abc22a1736
thanh nguyen <thanh.uou@gmail.com>
2021-04-19 20:19:34 +0900

    cannot deploy on vercel with environment variable

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
+const MONGO_DB_URL = 'mongodb+srv://thanh:thanh@cluster0.61tgr.mongodb.net/words?retryWrites=true&w=majority';
+
 const connectDB = handler => async (req, res) => {
     if(mongoose.connections[0].readyState){
         return handler(req, res);
     }
 
-    await mongoose.connect('mongodb+srv://thanh:thanh@cluster0.61tgr.mongodb.net/words?retryWrites=true&w=majority', {
+    await mongoose.connect(MONGO_DB_URL, {
         useUnifiedTopology: true,
         useFindAndModify: true,
         useCreateIndex: true,
