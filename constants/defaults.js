0517e3ba9464a5a23adc00e47d596338b2743a32
thanh90 <30317015+thanh90@users.noreply.github.com>
2021-09-14 21:25:32 +0900

    change to production server

--- constants/defaults.js
+++ constants/defaults.js
@@ -1,2 +1,2 @@
-// export const API_SERVER_ADDRESS = 'https://183.96.253.147:8053';
-export const API_SERVER_ADDRESS = 'https://localhost:8053';
+export const API_SERVER_ADDRESS = 'https://183.96.253.147:8053';
+// export const API_SERVER_ADDRESS = 'https://localhost:8053';
